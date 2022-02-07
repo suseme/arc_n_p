b4348df3d4bb010a3d03a82eb87340fd0a8120de
vin <vin@misday.com>
2022-02-07 14:34:23 +0800

    change content index

--- docs/_gitbook_plugin_analytics.js
+++ docs/_gitbook_plugin_analytics.js
@@ -7,12 +7,12 @@ if ('U44rX2nKCF') {
 	  s.parentNode.insertBefore(hm, s);
 	})();
 }
-if ('UA-28333273-1') {
+if ('UA-28333273-3') {
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-	ga('create', 'UA-28333273-1', 'auto');
+	ga('create', 'UA-28333273-3', 'auto');
 	ga('send', 'pageview');
 }
